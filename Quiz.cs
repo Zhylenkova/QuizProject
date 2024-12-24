ba01c2037844a07297e39f4890d85178cd43b8c8
Zhylenkova <olena.zhylenkova@gmail.com>
2024-12-24 09:18:02 +0100

    Add project files.

---
+++ Quiz.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizProject
+{
+    internal class Quiz
+    {
+        private Question[] _questions;
+        private int _score;
+
+        public Quiz(Question[] questions)
+        {
+            this._questions = questions;
+            _score = 0;
+
+        }
+
+        public void StartQuiz()
+        {
+            Console.WriteLine("Welcome!");
+            int questionNumber = 1;
+
+            foreach (Question question in _questions)
+            {
+                Console.WriteLine($"Question {questionNumber++}:");
+                DisplayQuestion(question);
+                int userChoice = GetUserChoice();
+                if (question.IsCorrectAnswer(userChoice))
+                {
+                    Console.WriteLine("Correct");
+                    _score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong, the correct answer was: {question.Answers[question.CorrectAnswerIndex]}");
+                }
+            }
+            DisplayResults();
+        }
+
+        private void DisplayResults()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Results                                 ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+
+            Console.WriteLine($"Quiz finished. Your score is: {_score} out of: {_questions.Length}");
+
+            double percentage = (double)_score / _questions.Length;
+
+        }
+
+        private void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Question                                ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+            Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < _questions.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan; // changes the text color
+                Console.Write("   ");
+                Console.Write(i + 1);
+                Console.ResetColor(); // resets the foreground (text) color
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer(number) : ");
+            string input = Console.ReadLine();
+            int choice = 0;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4: ");
+                input = Console.ReadLine();
+            }
+            return choice - 1; 
+         
+        }
+    }
+}
+
